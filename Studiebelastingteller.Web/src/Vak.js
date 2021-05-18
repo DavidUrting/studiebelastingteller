72b10e27bfd4d5a85bf39010d296a6d80215a163
davurt <david.urting@gmail.com>
2021-05-18 23:15:42 +0200

    - feedback (bij errors) - gebruik van adjacentHtml ipv innerHtml

--- Studiebelastingteller.Web/src/Vak.js
+++ Studiebelastingteller.Web/src/Vak.js
@@ -33,8 +33,22 @@ export class Vak {
         return this._naam;
     }
 
+    set naam(val) {
+        if (!val) {
+            throw "Een vaknaam moet minstens uit één teken bestaan";
+        }
+        this._naam = val;
+    }
+
     get studiepunten() {
         return this._studiepunten;
+    }
+
+    set studiepunten(val) {
+        if (isNaN(val) || val < 1) {
+            throw "Gelieve een getal groter dan 0 op te geven";
+        }
+        this._studiepunten = val;
     }
 
     // Het geschat aantal uren is een 'berekende' property.
@@ -49,7 +63,7 @@ export class Vak {
 
     set aantalUren(val) {
         if (isNaN(val) || val < 0) {
-            throw "Gelieve een getal groter dan 0 op te geven";
+            throw "Gelieve een getal groter dan of gelijk aan 0 op te geven";
         }
         this._aantalUren = val;
 
@@ -60,27 +74,92 @@ export class Vak {
     render(tbody) {
         let tr =
 `<tr id="vak-${this.id}">
-    <td>${this.naam}</td>
-    <td>${this.studiepunten}</td>
-    <td>${this.geschatAantalUren}</td>
-    <td><input type="number" value="${this.aantalUren}" readonly /></td>
+    <td><input name="naam" type="text" value="${this.naam}" class="form-control" /></td>
+    <td><input name="studiepunten" type="number" value="${this.studiepunten}" min="1" class="form-control" /></td>
+    <td><span>${this.geschatAantalUren}</span></td>
+    <td><input name="aantalUren" type="number" value="${this.aantalUren}" min="0" class="form-control" readonly /></td>
     <td>
         <button class="btn btn-primary">+</button>
         <button class="btn btn-danger float-end">x</button>
     </td>
 </tr>`;
-        tbody.innerHTML += tr;
+        // innerHTML gebruiken is gevaarlijk: want de tweede keer dat je een rij toevoegt zal de HTML content vervangen worden waardoor
+        // alle event handlers weggegooid worden...
+        tbody.insertAdjacentHTML('beforeend', tr);
 
-        let input = document.querySelector(`#vak-${this.id} input[type='number']`);
+        let naamInput = document.querySelector(`#vak-${this.id} input[name='naam']`);
+        naamInput.addEventListener("change", (evt) => {
+            try {
+                this._resetError(naamInput);
+
+                this.naam = evt.target.value;
+                this._vakkenlijst.save();
+            } catch (ex) {
+                this._setError(naamInput, ex);
+            }
+        });
+
+        let studiepuntenInput = document.querySelector(`#vak-${this.id} input[name='studiepunten']`);
+        let geschatAantalUrenSpan = document.querySelector(`#vak-${this.id} span`);
+        studiepuntenInput.addEventListener("change", (evt) => {
+            try {
+                this._resetError(studiepuntenInput);
+
+                this.studiepunten = evt.target.value;
+                this._geefFeedbackBijOverschrijding();
+                geschatAantalUrenSpan.innerText = this.geschatAantalUren;
+                this._vakkenlijst.save();
+            } catch (ex) {
+                this._setError(studiepuntenInput, ex);
+            }
+        });
+
+
+        let aantalUrenInput = document.querySelector(`#vak-${this.id} input[name='aantalUren']`);
+        aantalUrenInput.addEventListener("change", (evt) => {
+            try {
+                this._resetError(aantalUrenInput);
+
+                this.aantalUren = evt.target.value;
+                this._geefFeedbackBijOverschrijding();
+            } catch (ex) {
+                aantalUrenInput.value = this.aantalUren;
+                this._setError(aantalUrenInput, ex);
+            }            
+        });
+
         let verhoogAantalUrenButton = document.querySelector(`#vak-${this.id} > td:last-child > button:first-child`);
         verhoogAantalUrenButton.addEventListener("click", (evt) => {
             this.aantalUren++;
-            input.value = this.aantalUren;
+            aantalUrenInput.value = this.aantalUren;
+            this._geefFeedbackBijOverschrijding();
         });
 
         let deleteButton = document.querySelector(`#vak-${this.id} > td:last-child > button:last-child`);
         deleteButton.addEventListener("click", (evt) => {
             this._vakkenlijst.deleteVak(this.id);
         });
+
+        // En ook al direct een eerste keer aanroepen (want het zou kunnen dat het aantal uren al groter is dan het geschat aantal uren).
+        this._geefFeedbackBijOverschrijding();
+    }
+
+    _geefFeedbackBijOverschrijding() {
+        let aantalUrenInput = document.querySelector(`#vak-${this.id} input[name='aantalUren']`);
+        if (this.aantalUren > this.geschatAantalUren) {
+            aantalUrenInput.style.backgroundColor = "red";
+        } else {
+            aantalUrenInput.style.backgroundColor = "unset";
+        }
+    }
+
+    _resetError(input) {
+        input.setAttribute('title', '');
+        input.style.backgroundColor = "unset";
+    }
+
+    _setError(input, message) {
+        input.setAttribute('title', message);
+        input.style.backgroundColor = "red";
     }
 }
--- Studiebelastingteller.Web/src/index.js
+++ Studiebelastingteller.Web/src/index.js
@@ -3,11 +3,11 @@
 let vakkenlijst = new Vakkenlijst();
 
 document.getElementById("voegVakToe").addEventListener("click", function (evt) {
-    vakkenlijst.addVak("Test", 4);
+    vakkenlijst.addVak("Vak", 1);
 });
 
 document.getElementById("corrigeerUren").addEventListener("click", function (evt) {
-    let buttons = document.querySelectorAll("input[type='number']");
+    let buttons = document.querySelectorAll("input");
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].removeAttribute("readonly");
     }
